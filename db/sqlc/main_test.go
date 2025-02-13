1fd3e1c35529dcd78139d3b380022c688d435805
Otavio Tampellini <tampelliniotavio@gmail.com>
2025-02-13 10:42:09 -0300

    feat(config): variável de ambiente de conexão ao db

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,20 +6,16 @@ import (
 	"os"
 	"testing"
 
+	"github.com/TampelliniOtavio/backend-web-development-course/util"
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://username:password@localhost:5432/simple_bank?sslmode=disable"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../../")
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
--- main.go
+++ main.go
@@ -4,21 +4,20 @@ import (
 	"database/sql"
 	"log"
 
-	_ "github.com/lib/pq"
 	"github.com/TampelliniOtavio/backend-web-development-course/api"
 	db "github.com/TampelliniOtavio/backend-web-development-course/db/sqlc"
-)
-
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://username:password@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
+	"github.com/TampelliniOtavio/backend-web-development-course/util"
+	_ "github.com/lib/pq"
 )
 
 func main() {
+	config, err := util.LoadConfig(".")
 
-	var err error
-	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
@@ -27,7 +26,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 
 	if err != nil {
 		log.Fatal("cannot start server", err)
