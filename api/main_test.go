33e2e2a45f13d0ed9bac9f268177cf2c6bc90f7e
Otavio Tampellini <tampelliniotavio@gmail.com>
2025-02-17 16:07:57 -0300

    feat(user): login

--- api/main_test.go
+++ api/main_test.go
@@ -3,11 +3,26 @@ package api
 import (
 	"os"
 	"testing"
+	"time"
 
+	db "github.com/TampelliniOtavio/backend-web-development-course/db/sqlc"
+	"github.com/TampelliniOtavio/backend-web-development-course/util"
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/require"
 )
 
-func TestMain (m *testing.M) {
+func newTestServer(t *testing.T, store db.Store) *Server {
+	config := util.Config{
+		TokenSymmetricKey:   util.RandomString(32),
+		AccessTokenDuration: time.Minute,
+	}
+
+	server, err := NewServer(config, store)
+	require.NoError(t, err)
+	return server
+}
+
+func TestMain(m *testing.M) {
 	gin.SetMode(gin.TestMode)
 	os.Exit(m.Run())
 }
